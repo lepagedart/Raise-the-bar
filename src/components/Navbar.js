ed64fcfc140922ba48c36a604bf8ebe6d73ab6c2
lepagedart <jeremy.lepage@icloud.com>
2025-02-07 13:25:04 -0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
-import Link from 'next/link';
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <nav>
+      <Image src="/logo.png" alt="Raise the Bar Logo" width={150} height={50} />
       <ul>
-        <li><Link href="/">Home</Link></li>
-        <li><Link href="/services">Services</Link></li>
-        <li><Link href="/contact">Contact</Link></li>
+        <li><a href="/">Home</a></li>
+        <li><a href="/services">Services</a></li>
+        <li><a href="/contact">Contact</a></li>
       </ul>
     </nav>
   );
