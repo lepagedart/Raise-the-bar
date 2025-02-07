8e504cf1634f13aca0550d21a8598d6f420ebfcc
lepagedart <jeremy.lepage@icloud.com>
2025-02-07 14:13:59 -0500

    Create services.js

---
+++ pages/services.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+
+export default function Services() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Our Services</h1>
+      <p>We provide expert bar consulting, menu pricing, and staff training.</p>
+    </div>
+  );
+}
