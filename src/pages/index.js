2151ca143239a109be7fac1dcd16b20e273203f9
lepagedart <jeremy.lepage@icloud.com>
2025-02-07 12:00:58 -0500

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Raise the Bar Consulting</h1>
+      <p>Helping restaurants maximize their bar program efficiency.</p>
+    </div>
+  );
+}
