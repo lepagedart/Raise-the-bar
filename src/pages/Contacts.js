3cf32f1b72528c530f300d6d26a715cc342fc1b8
lepagedart <jeremy.lepage@icloud.com>
2025-02-07 12:19:04 -0500

    Update Contacts.js

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,6 +1,9 @@
+import Navbar from "../components/Navbar";
+
 export default function Contact() {
   return (
     <div>
+      <Navbar />
       <h1>Contact Us</h1>
       <p>Email us at contact@raisethebar.com</p>
     </div>
