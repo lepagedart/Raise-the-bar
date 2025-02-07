0da5462f5c03443d23c7d8a1aebbfc44bcce4b6a
lepagedart <jeremy.lepage@icloud.com>
2025-02-07 12:06:30 -0500

    Create services.js

---
+++ src/pages/services.js
@@ -0,0 +1,8 @@
+export default function Services() {
+  return (
+    <div>
+      <h1>Our Services</h1>
+      <p>We provide expert bar consulting, menu pricing, and staff training.</p>
+    </div>
+  );
+}
