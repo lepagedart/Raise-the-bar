238e90c26d83bff342590c696235d4319caacda0
lepagedart <jeremy.lepage@icloud.com>
2025-02-07 11:59:08 -0500

    Delete src/components/Index.js

--- src/components/Index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to Raise the Bar Consulting</h1>
-      <p>Helping restaurants maximize their bar program efficiency.</p>
-    </div>
-  );
-}
