d2fd345432a3ce7b4aaf1951b052ff0cd66f0be2
lepagedart <jeremy.lepage@icloud.com>
2025-02-07 14:16:20 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar"; // Import Navbar component
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar /> {/* Display the Navbar at the top */}
+      <h1>Welcome to Raise the Bar Consulting</h1>
+      <p>Helping restaurants maximize their bar program efficiency.</p>
+    </div>
+  );
+}
